c8b5708149495c9ee61320e3f0cdcd459eaeacc2
youssra <112857270+zombie0youssra@users.noreply.github.com>
2023-09-14 16:42:30 +0200

    al wat circels gemaakt

--- src/lib/slices/Members/index.svelte
+++ src/lib/slices/Members/index.svelte
@@ -4,13 +4,33 @@
 </script>
 
 <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	
-	{#each slice.items as item }
-	  <section>
-		<a href="{item.memberpage}">
-			<h3>{item.name[0].text}</h3>
-			<img src="{item.img.url}" alt="{item.img.alt}" width="50em" height="50em">
-		</a>
-	  </section>
+	{#each slice.items as item}
+		<section class="member-card">
+			<a href={item.memberpage}>
+				<h3>{item.name[0].text}</h3>
+				<img src={item.img.url} alt={item.img.alt} width="50em" height="50em" />
+			</a>
+		</section>
 	{/each}
 </section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		gap: 1rem;
+	}
+
+	.member-card {
+		background-color: #a675f5;
+		display: flex;
+		width: 10rem;
+		height: auto;
+		border-radius: 100%;
+	}
+
+	.member-card img {
+		width: auto;
+	}
+</style>
