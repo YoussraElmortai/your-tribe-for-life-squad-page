53b7bfa30da9437e468516e58cbe3fce1c627e2a
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-09-21 16:21:23 +0200

    begin hover implementeren

--- src/lib/slices/Members/index.svelte
+++ src/lib/slices/Members/index.svelte
@@ -3,19 +3,25 @@
 	export let slice;
 </script>
 
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
-	{#each slice.items as item}
-		<!-- {console.log(item)} -->
-		<section class="member-card">
-			<a href="/Member/{item.name[0].text.split(' ')[0].toLowerCase()}">
-				<h3>{@html prismic.asHTML(item.name)}</h3>
-				<img src={item.img.url} alt={item.img.alt} width="50em" height="50em" />
-			</a>
-		</section>
-	{/each}
-</section>
+<body>
+	<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+		{#each slice.items as item}
+			<!-- {console.log(item)} -->
+			<section class="member-card">
+				<a href="/Member/{item.name[0].text.split(' ')[0].toLowerCase()}">
+					<h3>{@html prismic.asHTML(item.name)}</h3>
+					<img src={item.img.url} alt={item.img.alt} width="50em" height="50em" />
+				</a>
+			</section>
+		{/each}
+	</section>
+</body>
 
 <style>
+	body:has(.member-card:hover) {
+		opacity: 0.5;
+	}
+
 	section {
 		display: flex;
 		flex-direction: row;
@@ -32,6 +38,11 @@
 		border-radius: 100%;
 		margin-top: 1rem;
 		overflow: hidden;
+	}
+
+	.member-card:hover {
+		scale: 200%;
+		opacity: 1;
 	}
 
 	.member-card a {
